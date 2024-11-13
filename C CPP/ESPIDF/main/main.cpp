eed04b4402485eaf46b88dd8ac3c3fcddaf0643a
kimdamitung <150353472+kimdamitung@users.noreply.github.com>
2024-11-13 18:48:48 +0700

    remove deriver mpu6050 pcf8575 st7735 write in CPP

---
+++ C CPP/ESPIDF/main/main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+//include driver espidf
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+//include user
+
+
+void app_main(void){
+    
+}
